25bdd12c01e69930be7fd4288ce3455da17cb54c
Dorayaki-Chan <zero.kotam@gmail.com>
2023-05-09 16:59:40 +0900

    ログファイルの修正

--- source/main/urg_library/vs2017/cpp/URGPDR5/URGPDR5.cpp
+++ source/main/urg_library/vs2017/cpp/URGPDR5/URGPDR5.cpp
@@ -1,4 +1,4 @@
-/*
+/*++*+*
 内容:
     URGplusURG8+socket4
 情報:2022/10/13 N.Hirota
@@ -38,13 +38,15 @@ TODO:
 
 #define DEFAULT_BUFLEN 2
 #define DEFAULT_PORT "5000"
-#define ESPIPADDR  "10.84.233.91"//"192.168.11.18"//192.168.208.233, 
+#define ESPIPADDR  "10.84.233.124" //"192.168.11.18"//192.168.208.233, 
 #define BUTTON_ID1 0
 #define FRAMESIZE  500
 #define STARTX 250 //歩行開始地点 250がLRFがある場所
 #define STARTY 250
 #define LRFX 250 //LRF地点 250がLRFがある場所
 #define LRFY 250
+
+#define LOG_PATH "Matsu-log-20230422-1.txt"
 
 
 
@@ -210,7 +212,7 @@ int myTXTout() {
     HANDLE hFile;
     DWORD dwWriteSize;
 
-    hFile = CreateFile(TEXT("test1115-1.txt"), GENERIC_READ | GENERIC_WRITE, 0, NULL,
+    hFile = CreateFile(TEXT(LOG_PATH), GENERIC_READ | GENERIC_WRITE, 0, NULL,
         OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hFile == INVALID_HANDLE_VALUE) {
         MessageBox(NULL, TEXT("ファイルが開けません"), NULL, MB_OK);
@@ -242,7 +244,7 @@ int MyPaint(HWND hwnd) {//描画は全部ここ
     HANDLE hFile;
     DWORD dwWriteSize;
 
-    hFile = CreateFile(TEXT("shiken1115-1.txt"), GENERIC_READ | GENERIC_WRITE, 0, NULL,
+    hFile = CreateFile(TEXT(LOG_PATH), GENERIC_READ | GENERIC_WRITE, 0, NULL,
         OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hFile == INVALID_HANDLE_VALUE) {
         //MessageBox(NULL, TEXT("ファイルが開けません"), NULL, MB_OK);
