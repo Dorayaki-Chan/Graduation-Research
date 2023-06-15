964d02d8a4b1c95430a826d4cc3e198d7576005c
Dorayaki-Chan <zero.kotam@gmail.com>
2023-06-16 01:57:13 +0900

    ログファイルの生成

--- source/PMW3901/src/main.py
+++ source/PMW3901/src/main.py
@@ -3,7 +3,17 @@ import time
 import argparse
 from pmw3901 import PMW3901, BG_CS_FRONT_BCM, BG_CS_BACK_BCM
 
+from openpyxl import Workbook
+import os
 
+# ワークブックを作成
+workbook = Workbook()
+
+# アクティブなシートを取得
+sheet = workbook.active
+
+# シート名を設定
+sheet.title = "Sheet1"
 
 # parser = argparse.ArgumentParser()
 # parser.add_argument('--board', type=str,
@@ -28,6 +38,8 @@ flo.set_rotation(0)     # Rotation of sensor in degrees
 tx = 0
 ty = 0
 
+lists = []
+
 try:
     while True:
         try:
@@ -37,6 +49,23 @@ try:
         tx += x
         ty += y
         print("Relative: x {:03d} y {:03d} | Absolute: x {:03d} y {:03d}".format(x, y, tx, ty))
+        lists.append([x, y, tx, ty])
         time.sleep(0.01)
 except KeyboardInterrupt:
+    # logフォルダーのパス
+    log_folder_path = "./log"
+    # logフォルダーが存在しない場合は作成する
+    if not os.path.exists(log_folder_path):
+        os.makedirs(log_folder_path)
+    
+    # セルに値を設定
+    for i, list in enumerate(lists):
+        sheet.cell(row=i+1, column=1).value = list[0]
+        sheet.cell(row=i+1, column=2).value = list[1]
+        sheet.cell(row=i+1, column=3).value = list[2]
+        sheet.cell(row=i+1, column=4).value = list[3]
+
+    # Excelファイルを保存
+    file_path = os.path.join(log_folder_path, "coordinates.xlsx")
+    workbook.save(file_path)
     pass
