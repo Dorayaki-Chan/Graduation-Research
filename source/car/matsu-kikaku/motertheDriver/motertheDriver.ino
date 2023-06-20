82e65b6dcd3669e6041a9f06eeb29c849a8378ca
Dorayaki-Chan <zero.kotam@gmail.com>
2023-06-20 20:05:17 +0900

    ヒロポン規格に直した

--- source/car/matsu-kikaku/motertheDriver/motertheDriver.ino
+++ source/car/matsu-kikaku/motertheDriver/motertheDriver.ino
@@ -20,17 +20,17 @@ const int MOTER_LEFT_FWD = 12;
 const int MOTER_LEFT_BWD = 13;
 
 void setup() {
-    // モーター右
-    pinMode(MOTER_RIGHT_EN, OUTPUT); 
-    pinMode(MOTER_RIGHT_FWD, OUTPUT);
-    pinMode(MOTER_RIGHT_BWD, OUTPUT);
+        // モーター右
+        pinMode(MOTER_RIGHT_EN, OUTPUT); 
+        pinMode(MOTER_RIGHT_FWD, OUTPUT);
+        pinMode(MOTER_RIGHT_BWD, OUTPUT);
 
-    // モーター左
-    pinMode(MOTER_LEFT_EN, OUTPUT);
-    pinMode(MOTER_LEFT_FWD, OUTPUT);
-    pinMode(MOTER_LEFT_BWD, OUTPUT);
+        // モーター左
+        pinMode(MOTER_LEFT_EN, OUTPUT);
+        pinMode(MOTER_LEFT_FWD, OUTPUT);
+        pinMode(MOTER_LEFT_BWD, OUTPUT);
 
-    Serial.begin(9600);
+        Serial.begin(9600);
 }
 
 void loop() {
@@ -60,18 +60,18 @@ void loop() {
                         }else if(incomingByteNum==3){//4byte目、正しい末尾文字ですか
                                 if(incomingByte == 255){//正しい末尾文字ですのでモーターを回します
                                         lastRecvTime=millis();
-                                        // 右
+                                        // 左
                                         if(motorSig[0]>127)
-                                                forword('r');
+                                                backword('l');
                                         else{
-                                                backword('r');
+                                                forword('l');
                                         }
                                         analogWrite(MOTER_RIGHT_EN, abs(motorSig[0]-127)*2);
-                                        // 左
+                                        // 右
                                         if(motorSig[1]>127)
-                                                forword('l');
+                                                backword('r');
                                         else{
-                                                backword('l');
+                                                forword('r');
                                         }
                                         analogWrite(MOTER_LEFT_EN,abs(motorSig[1]-127)*2);
                                         //Serial.println(abs(motorSig[1]-127)*2);
@@ -89,33 +89,33 @@ void loop() {
 }
 
 void forword(char rf){
-    if(rf == 'r'){
-        digitalWrite(MOTER_RIGHT_FWD, HIGH);
-        digitalWrite(MOTER_RIGHT_BWD, LOW);
-    }
-    else{
-        digitalWrite(MOTER_LEFT_FWD, HIGH);
-        digitalWrite(MOTER_LEFT_BWD, LOW);
-    }
+        if(rf == 'r'){
+                digitalWrite(MOTER_RIGHT_FWD, HIGH);
+                digitalWrite(MOTER_RIGHT_BWD, LOW);
+        }
+        else{
+                digitalWrite(MOTER_LEFT_FWD, HIGH);
+                digitalWrite(MOTER_LEFT_BWD, LOW);
+        }
 }
 
 void backword(char rf){
-    if(rf == 'r'){
-        digitalWrite(MOTER_RIGHT_FWD, LOW);
-        digitalWrite(MOTER_RIGHT_BWD, HIGH);
-    }
-    else{
-        digitalWrite(MOTER_LEFT_FWD, LOW);
-        digitalWrite(MOTER_LEFT_BWD, HIGH);
-    }
+        if(rf == 'r'){
+                digitalWrite(MOTER_RIGHT_FWD, LOW);
+                digitalWrite(MOTER_RIGHT_BWD, HIGH);
+        }
+        else{
+                digitalWrite(MOTER_LEFT_FWD, LOW);
+                digitalWrite(MOTER_LEFT_BWD, HIGH);
+        }
 }
 
 void mstop(){
-    digitalWrite(MOTER_RIGHT_FWD, HIGH);
-    digitalWrite(MOTER_RIGHT_BWD, HIGH);
-    analogWrite(MOTER_RIGHT_EN, 255);
+        digitalWrite(MOTER_RIGHT_FWD, HIGH);
+        digitalWrite(MOTER_RIGHT_BWD, HIGH);
+        analogWrite(MOTER_RIGHT_EN, 255);
 
-    digitalWrite(MOTER_LEFT_FWD, HIGH);
-    digitalWrite(MOTER_LEFT_BWD, HIGH);
-    analogWrite(MOTER_LEFT_EN, 255);
+        digitalWrite(MOTER_LEFT_FWD, HIGH);
+        digitalWrite(MOTER_LEFT_BWD, HIGH);
+        analogWrite(MOTER_LEFT_EN, 255);
 }
