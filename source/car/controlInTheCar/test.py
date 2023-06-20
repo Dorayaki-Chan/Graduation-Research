f3fdb9f6edd4d7aa2aaa4de24f00611f683b69b4
Dorayaki-Chan <zero.kotam@gmail.com>
2023-06-20 13:46:05 +0900

    角度計算

---
+++ source/car/controlInTheCar/test.py
@@ -0,0 +1,23 @@
+# 目標座標との角度を計算するプログラム
+import math
+
+now_x = 0
+now_y = 0
+now_rotation = -90
+
+def howRotation(x, y):
+    dif_x = x - now_x
+    dif_y = y - now_y
+    
+    return math.degrees(math.atan2(dif_y, dif_x))
+
+def howDiffRotation(rotation):
+    dif_rotation = rotation - now_rotation
+    if dif_rotation > 180:
+        dif_rotation -= 360
+    elif dif_rotation < -180:
+        dif_rotation += 360
+    return dif_rotation
+
+print(howRotation(-1, -1))
+print(howDiffRotation(howRotation(-1, -1)))
