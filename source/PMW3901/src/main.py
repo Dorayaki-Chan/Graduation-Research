ff97ece1cc3a29a440963fee7ddb4a8596a93883
Dorayaki-Chan <zero.kotam@gmail.com>
2023-06-20 12:13:10 +0900

    経過時間加えた

--- source/PMW3901/src/main.py
+++ source/PMW3901/src/main.py
@@ -15,21 +15,8 @@ sheet = workbook.active
 # シート名を設定
 sheet.title = "Sheet1"
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--board', type=str,
-#                     choices=['pmw3901', 'paa5100'],
-#                     required=True,
-#                     help='Breakout type.')
-# parser.add_argument('--rotation', type=int,
-#                     default=0, choices=[0, 90, 180, 270],
-#                     help='Rotation of sensor in degrees.')
-# parser.add_argument('--spi-slot', type=str,
-#                     default='front', choices=['front', 'back'],
-#                     help='Breakout Garden SPI slot.')
-# args = parser.parse_args()
-
 # Pick the right class for the specified breakout
-SensorClass = PMW3901 # if args.board == 'pmw3901' else PAA5100
+SensorClass = PMW3901 
 
 flo = SensorClass(spi_port=0, spi_cs_gpio=BG_CS_FRONT_BCM)
 flo.set_rotation(0)     # Rotation of sensor in degrees
@@ -40,6 +27,13 @@ ty = 0
 
 lists = []
 
+# 経過時間を計測するために開始時刻を保存
+start = time.time()
+
+# 経過時間を秒で返す
+def get_elapsed_time():
+    return time.time() - start
+
 try:
     while True:
         try:
@@ -48,8 +42,10 @@ try:
             continue
         tx += x
         ty += y
-        print("Relative: x {:03d} y {:03d} | Absolute: x {:03d} y {:03d}".format(x, y, tx, ty))
-        lists.append([x, y, tx, ty])
+        # 経過時間を表示
+        elapsed_time = time.time() - start
+        print("Elapsed_time:{0} [sec] | Relative: x {:03d} y {:03d} | Absolute: x {:03d} y {:03d}".format(get_elapsed_time(), x, y, tx, ty))
+        lists.append([get_elapsed_time(), x, y, tx, ty])
         time.sleep(0.01)
 except KeyboardInterrupt:
     # logフォルダーのパス
