b8910c510dc6c9377b05684849d8e30e7b2c9088
Dorayaki-Chan <zero.kotam@gmail.com>
2023-06-24 00:46:57 +0900

    ログクラス完成

--- source/main/Asuka/soft/log.py
+++ source/main/Asuka/soft/log.py
@@ -1,16 +1,18 @@
+#!/usr/bin/env python
+
 """
 内容:
 	ログファイルを生成するクラス
 情報:2023/06/23  K.Matsumoto
 """
-
 import os
 import time 
 import csv
 
-# logクラス
 class Logs:
+    """ログファイルを生成するクラス"""
     def __init__(self):
+        self.startTime = 0
         self.alllogs = []
         self.pointlogs = []
         # logフォルダーのパス
@@ -20,24 +22,50 @@ class Logs:
             os.makedirs(self.log_folder_path)
 
     def start(self):
-        # 経過時間を計測するために開始時刻を保存
-        self.start = time.time()
+        """経過時間の計測を開始する."""
+        self.startTime = time.time()
     
-    def addAll(self, time, ax, ay, aa, tx, ty, ta):
-        self.alllogs.append({'time': time, 'ax': ax, 'ay': ay, 'aa': aa, 'tx': tx, 'ty': ty, 'ta': ta})
+    def addAll(self, ax, ay, aa, tx, ty, ta):
+        """全ての座標を追加する.
+
+        Args:
+            ax (float): 絶対x座標
+            ay (float): 絶対y座標
+            aa (float): 絶対の傾き
+            tx (float): 相対x座標
+            ty (float): 相対y座標
+            ta (float): 総体の傾き
+        """
+        nowTime = self.startTime if self.startTime != 0 else self.__get_elapsed_time()
+        self.alllogs.append({'time': nowTime, 'ax': ax, 'ay': ay, 'aa': aa, 'tx': tx, 'ty': ty, 'ta': ta})
     
-    def addPoint(self, time, ax, ay, aa, tx, ty, ta, order):
-        self.pointlogs.append({'time': time, 'ax': ax, 'ay': ay, 'aa': aa, 'tx': tx, 'ty': ty, 'ta': ta, 'order': order})
+    def addPoint(self, ax, ay, aa, tx, ty, ta, order):
+        """重要ポイントの座標を追加する.
+
+        Args:
+            ax (float): 絶対x座標
+            ay (float): 絶対y座標
+            aa (float): 絶対の傾き
+            tx (float): 相対x座標
+            ty (float): 相対y座標
+            ta (float): 総体の傾き
+            order (String): 説明
+        """
+        nowTime = self.startTime if self.startTime != 0 else self.__get_elapsed_time()
+        self.pointlogs.append({'time': nowTime, 'ax': ax, 'ay': ay, 'aa': aa, 'tx': tx, 'ty': ty, 'ta': ta, 'order': order})
     
     def showAll(self):
+        """全てのログを表示する."""
         for log in self.alllogs:
             print(log)
 
     def showPoint(self):
+        """重要ポイントのログを表示する."""
         for log in self.pointlogs:
             print(log)
 
     def makeCSV(self):
+        """全ログファイルを生成する."""
         # ファイル名を作成
         now = time.strftime('%Y%m%d_%H%M%S')
         filename = 'OpticalFlow_All-' + now + '.csv'
@@ -49,6 +77,7 @@ class Logs:
                 writer.writerow(log)
 
     def makeTXT(self):
+        """重要ポイントのログファイルを生成する."""
         # ファイル名を作成
         now = time.strftime('%Y%m%d_%H%M%S')
         filename = 'OpticalFlow_Point-' + now + '.txt'
@@ -57,3 +86,7 @@ class Logs:
         with open(file_path, 'w') as f:
             for log in self.pointlogs:
                 f.write(str(log) + '\n')
+
+    def __get_elapsed_time(self):
+        """時間を秒で返す"""
+        return round(time.time() - self.startTime, 4)
